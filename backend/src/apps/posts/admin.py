2efd177b1cbb817368e489e10bb982e56e7e78d8
Yurii <yuriydorosh2005@gmail.com>
2024-05-11 17:35:52 +0300

    UPDATE: posts admin(posts and comments)

---
+++ backend/src/apps/posts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from apps.posts.models import Posts, Comments
+
+
+@admin.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user_id", "date_added")
+    list_filter = ("id", "user_id", "title")
+    search_fields = ("title", "user_id")
+    
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "post_id", "comment_body")
+    list_filter = ("id", "user_id", "post_id")
+    search_fields = ("post_id", "user_id")
